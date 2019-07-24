3cc43b112a47e715deec1313a27c7ec3de9759f7
Jean-Pierre Carvalho <jota.fullstack.pt@gmail.com>
2019-07-24 13:24:32 +0100

    Add Boostrap + Babel

--- src/App.js
+++ src/App.js
@@ -7,10 +7,15 @@ import Header from "./components/header";
 // Footer
 import Footer from "./components/footer";
 
+
+
+import { Button } from 'react-bootstrap';
+
 function App() {
   return (
     <div className="App">
       <Header />
+       <Button variant="success">Success</Button>
       <Footer />
     </div>
   );
