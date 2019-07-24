3494b56384a60ecaf26226de6da6456ebdf21ce7
Jean-Pierre Carvalho <jota.fullstack.pt@gmail.com>
2019-07-24 17:53:53 +0100

    HeaderConfig

--- src/App.js
+++ src/App.js
@@ -9,32 +9,41 @@ import Header from "./components/header";
 
 //import { Button } from "react-bootstrap";
 
+var HeaderConfig = {
+  isShow: true,
+  theme: "light",
+  brandShow: true,
+  brand: { title: "Logo", url: "" },
+  menuShow: true,
+  linksShow: true,
+  links: [
+    { title: "Products", url: "", subItemShow: false },
+    { title: "Agency", url: "", subItemShow: false },
+    { title: "Docs", url: "", subItemShow: false },
+    {
+      title: "Contact Us",
+      url: "",
+      subItemShow: false,
+      subItens: [{ title: "Teste", url: "" }, { title: "fcgvnbhmjn", url: "" }]
+    }
+  ],
+  ctaShow: false,
+  cta: { title: "More Information", url: "", variant: "primary" }
+};
+
 function App() {
   return (
     <div className="App">
       <Header
-        isShow={true}
-        theme={"light"}
-        brandShow={true}
-        brand={{ title: "Logo", url: "" }}
-        menuShow={true}
-        linksShow={true}
-        links={[
-          { title: "Products", url: "", subItemShow: false },
-          { title: "Agency", url: "", subItemShow: false },
-          { title: "Docs", url: "", subItemShow: false },
-          {
-            title: "Contact Us",
-            url: "",
-            subItemShow: false,
-            subItens: [
-              { title: "Teste", url: "" },
-              { title: "fcgvnbhmjn", url: "" }
-            ]
-          }
-        ]}
-        ctaShow={false}
-        cta={{ title: "More Information", url: "", variant: "primary" }}
+        isShow={HeaderConfig.isShow}
+        theme={HeaderConfig.theme}
+        brandShow={HeaderConfig.brandShow}
+        brand={HeaderConfig.brand}
+        menuShow={HeaderConfig.menuShow}
+        linksShow={HeaderConfig.linksShow}
+        links={HeaderConfig.links}
+        ctaShow={HeaderConfig.ctaShow}
+        cta={HeaderConfig.cta}
       />
       {/*<Button variant="success">Success</Button>
       <Footer />*/}
