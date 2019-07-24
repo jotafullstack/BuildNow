6e140c60a4961f59e9c2bf223a8239279950908b
Jean-Pierre Carvalho <jota.fullstack.pt@gmail.com>
2019-07-24 22:28:31 +0100

    Builder Tooltip: add mouse leave, mouse enter and component

--- src/components/main/BuilderTooltip/builderTooltip-container.js
+++ src/components/main/BuilderTooltip/builderTooltip-container.js
@@ -5,7 +5,16 @@ import { Button } from "react-bootstrap";
 class BuilderTooltip extends Component {
   render() {
     return (
-      <div className={this.props.builder ? "builder-tooltip" : null}>
+      <div
+        style={{
+          position: "absolute",
+          margin: "auto",
+          left: "0px",
+          top: "0px",
+          left: "0px",
+          right: "0px"
+        }}
+      >
         <Button variant="primary">Edit</Button>
         <Button variant="primary">Remove</Button>
       </div>
--- src/components/main/Title/title-container.js
+++ src/components/main/Title/title-container.js
@@ -5,14 +5,31 @@ import { Row, Col } from "react-bootstrap";
 import BuilderTooltip from "../BuilderTooltip";
 
 class Title extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { over: true };
+  }
+
+  mouseOver() {
+    console.log("Mouse over!!!");
+    this.setState({ over: true });
+  }
+
+  mouseOut() {
+    console.log("Mouse out!!!");
+    this.setState({ over: false });
+  }
+
   render() {
     return (
-      {/* If Builder Mode is ON appears one border in component*/}
-      <Row className={this.props.builder ? "Builder-Hoverable" : null}>
+      <Row
+        className={this.props.builder ? "Builder-Hoverable" : null}
+        onMouseLeave={() => this.mouseOut()}
+        onMouseEnter={() => this.mouseOver()}
+      >
         <Col xs={12} sm={12} md={12} lg={12}>
           <h1>{this.props.description}</h1>
-          {/* If Builder Mode is ON appears ToolTip*/}
-          {this.props.builder ? <BuilderTooltip /> : null}
+          {this.state.over ? <BuilderTooltip /> : null}
         </Col>
       </Row>
     );
