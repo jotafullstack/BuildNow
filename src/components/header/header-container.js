506d8924211fe87b2ea0abe4bd58f6cd639274b8
Jean-Pierre Carvalho <jota.fullstack.pt@gmail.com>
2019-07-25 18:31:36 +0100

    Add redux function with Button

--- src/components/header/header-container.js
+++ src/components/header/header-container.js
@@ -8,60 +8,60 @@ import { connect } from "react-redux";
 /* LinkMenu Component Props its title, url, subItemShow, subItens */
 import LinkMenu from "./LinkMenu";
 
-class Header extends Component {
-  /* Construtor for getting props and setting states */
-  constructor(props) {
-    super(props);
-  }
-
-  /* Render Component */
-  render() {
-    return (
-      <header>
-        {/* Show or Hide Header */}
-        {this.props.HeaderConfig.isShow ? (
-          <Navbar bg={this.props.HeaderConfig.theme} expand="lg">
-            {/* Brand Logo */}
-            {this.props.HeaderConfig.brandShow ? (
-              <Navbar.Brand href={this.props.HeaderConfig.brand.url}>
-                {this.props.HeaderConfig.brand.title}
-              </Navbar.Brand>
-            ) : null}
-            {/* Add Menu Toogle in Mobile*/}
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            {/* Show or hide links */}
-            {this.props.HeaderConfig.menuShow ? (
-              <Navbar.Collapse id="basic-navbar-nav">
-                {/* ml-auto para alinhar à direita, mr-auto para alinhar à esquerda */}
-                <Nav className="ml-auto">
-                  {this.props.HeaderConfig.linksShow
-                    ? this.props.HeaderConfig.links.map(link => {
-                        /* Return a single link */
-                        return (
-                          <LinkMenu
-                            key={link.title}
-                            title={link.title}
-                            url={link.url}
-                            subItemShow={link.subItemShow}
-                            subItens={link.subItens}
-                          />
-                        );
-                      })
-                    : null}
-                  {/* Show or Hide Call To Action */}
-                  {this.props.HeaderConfig.ctaShow ? (
-                    <Button variant={this.props.HeaderConfig.cta.variant}>
-                      {this.props.HeaderConfig.cta.title}
-                    </Button>
-                  ) : null}
-                </Nav>
-              </Navbar.Collapse>
-            ) : null}
-          </Navbar>
-        ) : null}
-      </header>
-    );
-  }
+function headerActive(isShow) {
+  return {
+    type: "HEADER_ACTIVE",
+    isShow
+  };
 }
+
+const Header = ({ HeaderConfig, dispatch }) => (
+  <header>
+    {/* Show or Hide Header */}
+    {HeaderConfig.isShow ? (
+      <Navbar bg={HeaderConfig.theme} expand="lg">
+        {/* Brand Logo */}
+        {HeaderConfig.brandShow ? (
+          <Navbar.Brand href={HeaderConfig.brand.url}>
+            {HeaderConfig.brand.title}
+          </Navbar.Brand>
+        ) : null}
+        {/* Add Menu Toogle in Mobile*/}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        {/* Show or hide links */}
+        {HeaderConfig.menuShow ? (
+          <Navbar.Collapse id="basic-navbar-nav">
+            {/* ml-auto para alinhar à direita, mr-auto para alinhar à esquerda */}
+            <Nav className="ml-auto">
+              {HeaderConfig.linksShow
+                ? HeaderConfig.links.map(link => {
+                    /* Return a single link */
+                    return (
+                      <LinkMenu
+                        key={link.title}
+                        title={link.title}
+                        url={link.url}
+                        subItemShow={link.subItemShow}
+                        subItens={link.subItens}
+                      />
+                    );
+                  })
+                : null}
+              {/* Show or Hide Call To Action */}
+              {HeaderConfig.ctaShow ? (
+                <Button
+                  variant={HeaderConfig.cta.variant}
+                  onClick={() => dispatch(headerActive(HeaderConfig.isShow))}
+                >
+                  {HeaderConfig.cta.title}
+                </Button>
+              ) : null}
+            </Nav>
+          </Navbar.Collapse>
+        ) : null}
+      </Navbar>
+    ) : null}
+  </header>
+);
 
 export default connect(state => ({ HeaderConfig: state.HeaderConfig }))(Header);
--- src/store/index.js
+++ src/store/index.js
@@ -1,57 +1,64 @@
 import { createStore } from "redux";
 
-function reducer() {
-  return {
-    HeaderConfig: {
-      isShow: true,
-      theme: "light",
-      brandShow: true,
-      brand: { title: "Logo", url: "" },
-      menuShow: true,
-      linksShow: true,
-      links: [
-        {
-          title: "Qualquer Coisa",
-          url: "",
-          subItemShow: false,
-          subItens: [
-            { title: "Teste 1", url: "" },
-            { title: "Teste 2", url: "" }
-          ]
-        },
-        { title: "Agency", url: "", subItemShow: false, subItens: [] },
-        { title: "Docs", url: "", subItemShow: false, subItens: [] },
-        {
-          title: "Contact Us",
-          url: "",
-          subItemShow: false,
-          subItens: [
-            { title: "Teste 1", url: "" },
-            { title: "Teste 2", url: "" }
-          ]
-        }
-      ],
-      ctaShow: false,
-      cta: { title: "More Information", url: "", variant: "primary" }
-    },
-    MainConfig: {
-      // Builder Mode
-      builder: true,
-      isShow: true,
-      fluid: true,
-      titleShow: true,
-      title: {
-        description: "Title"
+const INITIAL_STATE = {
+  HeaderConfig: {
+    isShow: true,
+    theme: "light",
+    brandShow: true,
+    brand: { title: "Logo", url: "" },
+    menuShow: true,
+    linksShow: true,
+    links: [
+      {
+        title: "Qualquer Coisa",
+        url: "",
+        subItemShow: false,
+        subItens: [{ title: "Teste 1", url: "" }, { title: "Teste 2", url: "" }]
       },
-      subTitleShow: true,
-      subTitle: {
-        description: "Subtitle"
+      { title: "Agency", url: "", subItemShow: false, subItens: [] },
+      { title: "Docs", url: "", subItemShow: false, subItens: [] },
+      {
+        title: "Contact Us",
+        url: "",
+        subItemShow: false,
+        subItens: [{ title: "Teste 1", url: "" }, { title: "Teste 2", url: "" }]
       }
+    ],
+    ctaShow: true,
+    cta: { title: "More Information", url: "", variant: "primary" }
+  },
+  MainConfig: {
+    // Builder Mode
+    builder: true,
+    isShow: true,
+    fluid: true,
+    titleShow: true,
+    title: {
+      description: "Title"
     },
-    FooterConfig: {
-      isShow: true
+    subTitleShow: true,
+    subTitle: {
+      description: "Subtitle"
     }
-  };
+  },
+  FooterConfig: {
+    isShow: true
+  }
+};
+
+function reducer(state = INITIAL_STATE, action) {
+  console.log(action);
+  if (action.type === "HEADER_ACTIVE") {
+    console.log(state);
+    return {
+      ...state,
+      HeaderConfig: {
+        ...state.HeaderConfig,
+        isShow: !action.isShow
+      }
+    };
+  }
+  return state;
 }
 
 const store = createStore(reducer);
